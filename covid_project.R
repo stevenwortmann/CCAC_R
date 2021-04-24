d9654f5bc8a24d8ae1deda8f21f8544eddbfa3c6
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-04-23 23:28:32 -0400

    Added basic filter for US data to start.

--- covid_project.R
+++ covid_project.R
@@ -1,3 +1,10 @@
 library(tidyverse)
 
-data <- as_tibble(read.csv('/users/steve_wortmann/desktop/dat204/final/owid-covid-data.csv'))
+data <- as_tibble(read.csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'))
+
+unique(data$location)
+
+data %>% 
+  select(iso_code, continent, location, date, total_cases, new_cases, total_deaths, new_deaths) %>%
+  filter(grepl('United States', location)) %>% arrange(desc(date))
+
