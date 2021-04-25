6696d63361b628263d83e4a2122bb76644301dd9
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-04-25 14:02:36 -0400

    Began preliminary t-tests comparing top 10% of deaths with fields: pop. density, gdp/capita, median age, human dev. index.

--- covid_project.R
+++ covid_project.R
@@ -4,25 +4,34 @@ url <- 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/
 data <- as_tibble(read.csv(url))
 
 colnames(data)
-
 unique(data$location)
-unique(data$continent)[-2]
 
 data %>% # Current United States data
   select(iso_code, continent, location, date, total_cases, new_cases, total_deaths, new_deaths) %>%
   filter(grepl('United States', location)) %>% arrange(desc(date))
 
-data %>% # Current total cases, deaths, median age all countries
-  select(location, date, total_cases, total_cases_per_million, total_deaths, total_deaths_per_million, median_age) %>%
+countries <- data %>% # Key data for all countries
+  select(location, date, total_cases, total_cases_per_million, total_deaths, total_deaths_per_million, population_density, gdp_per_capita, median_age, human_development_index) %>%
   filter(grepl((Sys.Date()-1),date)) %>% arrange(desc(total_deaths_per_million))
 
-high_death <- data %>% # Current total cases, deaths, median age all countries - sorted most deaths
-  select(location, date, total_cases, total_cases_per_million, total_deaths, total_deaths_per_million, median_age) %>%
-  filter(grepl((Sys.Date()-1),date)) %>% arrange(desc(total_deaths_per_million))
+countries
 
-high_age <- data %>% # Current total cases, deaths, median age all countries - sorted highest age
-  select(location, date, total_cases, total_cases_per_million, total_deaths, total_deaths_per_million, median_age) %>%
-  filter(grepl((Sys.Date()-1),date)) %>% arrange(desc(median_age))
+colnames(countries) # Total cases and deaths per million, population density, GDP/capita, median age, human dev. index
 
-# T-test comparing top-50 fatalities to top-50 median age
-t.test(high_death$total_deaths_per_million[1:50], high_age$total_deaths_per_million[1:50])
+# T-test: top-20 deaths/million vs. median age
+t.test((countries%>%arrange(desc(median_age)))$total_deaths_per_million[1:20],
+       (countries%>%arrange(desc(total_deaths_per_million)))$total_deaths_per_million[1:20])
+
+# T-test: top-20 deaths/million vs. population density
+t.test((countries%>%arrange(desc(population_density)))$total_deaths_per_million[1:20],
+       (countries%>%arrange(desc(total_deaths_per_million)))$total_deaths_per_million[1:20])
+
+# T-test: top-20 deaths/million vs. human dev. index (lowest)
+t.test((countries%>%arrange(human_development_index))$total_deaths_per_million[1:20],
+       (countries%>%arrange(desc(total_deaths_per_million)))$total_deaths_per_million[1:20])
+
+# T-test: top-20 deaths/million vs. gdp/capita (lowest)
+t.test((countries%>%arrange(gdp_per_capita))$total_deaths_per_million[1:20],
+       (countries%>%arrange(desc(total_deaths_per_million)))$total_deaths_per_million[1:20])
+
+countries%>%arrange((human_development_index))
