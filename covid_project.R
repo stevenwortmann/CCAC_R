9a447dba17d73d6b2425d2b12b84e094b12edd92
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-05-01 17:32:11 -0400

    Began visual graphing analysis for USA covid data.

--- covid_project.R
+++ covid_project.R
@@ -6,9 +6,9 @@ data <- as_tibble(read.csv(url))
 colnames(data)
 unique(data$location)
 
-data %>% # Current United States data
-  select(iso_code, continent, location, date, total_cases, new_cases, total_deaths, new_deaths) %>%
-  filter(grepl('United States', location)) %>% arrange(desc(date))
+usa <- data %>% # Current United States data
+  select(iso_code, continent, location, date, total_cases, new_cases, total_deaths, total_deaths_per_million, new_deaths) %>%
+  filter(grepl('United States', location)) #%>% arrange(desc(date))
 
 countries <- data %>% # Key data for all countries
   select(location, date, total_cases, total_cases_per_million, total_deaths, total_deaths_per_million, population_density, gdp_per_capita, median_age, human_development_index, aged_65_older, aged_70_older) %>%
@@ -41,3 +41,11 @@ t.test((countries%>%arrange(aged_70_older))$total_deaths_per_million[1:20],
 # T-test: Comparing fatalities of 20 lowest GDP/capital and 20 oldest median age countries...
 t.test((countries%>%arrange(gdp_per_capita))$total_deaths_per_million[1:20],
        (countries%>%arrange(desc(aged_70_older)))$total_deaths_per_million[1:20])
+
+ggplot(usa, aes(x=date, y=total_deaths, color="#990239")) + geom_point() +
+  xlab('Time') + ylab('Total Deaths') + theme(legend.position = "none") +
+  ggtitle('USA: Covid Deaths')
+
+ggplot(usa, aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
+  xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
+  ggtitle('USA: Covid New Deaths (Daily)')
