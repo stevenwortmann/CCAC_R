0f326a1a57db7d7db946ea71854434d94cb33860
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-05-10 00:00:43 -0400

    Began three-tiered input structure for server/ui interaction.

--- covid_app.R
+++ covid_app.R
@@ -26,12 +26,9 @@ ggplotly(ggplot(subset(data, location %in% top_20), aes(x=date, y=new_deaths_smo
 
 
 ui <- function(input, output) {# Fill in the spot we created for a plot
-  fluidPage(    
-    # Give the page a title
+  fluidPage(
     titlePanel("Covid Data by Country"),
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      # Define the sidebar with one input
+    sidebarLayout(
       sidebarPanel(
         selectInput("country", "Region:", 
                     choices=country_list,),hr(),
@@ -39,17 +36,23 @@ ui <- function(input, output) {# Fill in the spot we created for a plot
                     choices=colnames(data),),hr(),
         selectInput("rates", "Data 2:", 
                     choices=colnames(data),),hr(),
-        helpText("Data from AT&T (1961) The World's Telephones.")),
-      # Create a spot for the barplot
+        helpText("Data from www.ourworldindata.org")),
+      
       mainPanel(plotlyOutput("phonePlot"))
     )
   )}
 
 server <-function(input, output) {
-    
+  
+  url <- 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
+  data <- as_tibble(read.csv(url))
+  data$date <- as.Date(data$date, '%Y-%m-%d')
+  
+  country <- reactive({subset(data, location== input$country)})
+  
     # Fill in the spot we created for a plot
     output$phonePlot <- renderPlotly({
-      ggplotly(ggplot(countries, aes(x=date, y=input$rates, color=location)) + 
+      ggplotly(ggplot(country, aes(x=date, y=input$rates, color=location)) + 
         geom_line() + xlab('Time') + ylab('New Deaths/Million') + #ylim(0,4500) + 
         ggtitle('Daily Covid Deaths per Million, Worldwide') + theme(legend.position = "bottom") + 
         scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1)))))
