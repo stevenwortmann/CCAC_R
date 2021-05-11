1538b032d23ce99d89f50b97750b6b1927445d84
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-05-10 23:09:57 -0400

    Began distilling dataset to those relevant for report.

--- covid_project.R
+++ covid_project.R
@@ -1,42 +1,42 @@
 library(tidyverse)
 library(plotly)
-library(rsconnect)
 
 url <- 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-data <- as_tibble(read.csv(url))
+data <- as_tibble(read.csv(url)) %>% select(-iso_code,-continent)
 data$date <- as.Date(data$date, '%Y-%m-%d')
 
 colnames(data)
 unique(data$location)
 
-continents <- c('North America', 'Europe', 'European Union', 'South America','Asia', 'Africa')
+continents <- c('World','North America','Europe','European Union','South America','Asia','Africa')
 
-# All aggregated world data
-world <- data %>% filter((location %in% 'World')) %>%
-  select(-iso_code,-continent) %>% arrange(desc(date))
-
-# All aggregated continental data
-continental <- data %>% filter((location %in% continents)) %>%
-  select(-iso_code,-continent) %>% arrange(desc(date))
-
-# All USA data
-usa <- data %>% filter((location %in% 'United States')) %>%
-  select(-iso_code,-continent) %>% arrange(desc(date))
-
-# Current USA data
-usa %>% filter(grepl((Sys.Date()-1),date))
-
-# Current countries data, ranked most new deaths per million
 countries <- data %>% filter(!(location %in% continents)) %>%
-  select(-iso_code,-continent) %>% filter(grepl((Sys.Date()-1),date)) %>% arrange(desc(new_deaths_per_million))
+  filter(grepl((Sys.Date()-1),date))
 
-# top 10 countries by fatality
-top_10 <- (countries%>%arrange(desc(total_deaths)) %>% 
-             filter(!(location %in% continents)))$location[1:10]
+countries
 
-# top 20 countries by fatality
-top_20 <- (countries%>%arrange(desc(total_deaths)) %>% 
-             filter(!(location %in% continents)))$location[1:20]
+# Top 10 countries by deaths: 
+top_10_deaths <- (countries%>%arrange(desc(total_deaths)))$location[1:10]
+top_10_deaths
+
+#Top 10 countries by deaths-per-million:
+top_10_deathsPerMillion <- (countries%>%arrange(desc(total_deaths_per_million)))$location[1:10]
+top_10_deathsPerMillion
+
+# Plot 1: Top-10 fatality countries, new deaths over time
+ggplot(subset(data, location %in% top_10_deaths), aes(x=date, y=new_deaths_smoothed, color=location,na.rm=T)) +
+  geom_line() + ylab('New Deaths') + ylim(0,3500) + ggtitle('Top 10 Countries: New Deaths') + 
+  theme(axis.title.x=element_blank(), legend.position = "bottom") +
+  scale_x_date(date_breaks = '3 month',date_labels = "%b%y",limits = as.Date(c('2020-03-01','2021-05-01')))
+
+ggplotly(ggplot( # Interactive version of plot 1
+  subset(data, location %in% top_10_deaths), aes(x=date, y=new_deaths_smoothed, color=location,na.rm=T)) +
+    geom_line() + ylab('New Deaths') + ggtitle('Top 10 Countries: New Deaths') + 
+    theme(axis.title.x=element_blank(), legend.position = "none") +
+    scale_x_date(date_breaks = '1 month',date_labels = "%b%y",limits = as.Date(c('2020-02-01',(Sys.Date()-1)))))
+
+countries %>% select(date,location,total_deaths_per_million) %>% arrange(desc(total_deaths_per_million))
+write.csv(countries, '/Users/steve_wortmann/Desktop/dat204/final/sample.csv')
 
 # T-test: Comparing fatalities of lowest and highest median age countries...
 t.test((countries%>%arrange((median_age)))$total_deaths_per_million[1:20],
@@ -67,39 +67,54 @@ ggplot(usa, aes(x=date, y=total_deaths, color="#990239")) + geom_point() +
   ggtitle('USA: Covid Deaths')
 
 ggplotly(ggplot(data %>% filter(grepl('United States', location)), 
-  aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
-  xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
-  ggtitle('USA: Covid New Deaths (Daily)'))
+                aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
+           xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
+           ggtitle('USA: Covid New Deaths (Daily)'))
 
 ggplotly(ggplot(data %>% filter(grepl('Brazil', location)), 
-  aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
-  xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
-  ggtitle('Brazil: Covid New Deaths (Daily)'))
+                aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
+           xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
+           ggtitle('Brazil: Covid New Deaths (Daily)'))
 
 ggplotly(ggplot(data %>% filter(grepl('India', location)), 
-  aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
-  xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
-  ggtitle('India: Covid New Deaths (Daily)'))
+                aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
+           xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
+           ggtitle('India: Covid New Deaths (Daily)'))
 
 ggplotly(ggplot(data %>% filter(grepl('India', location)), 
-  aes(x=date, y=new_cases, color="#990239")) + geom_point() +
-  xlab('Time') + ylab('New Cases') + theme(legend.position = "none") +
-  ggtitle('India: Covid New Cases (Daily)'))
+                aes(x=date, y=new_cases, color="#990239")) + geom_point() +
+           xlab('Time') + ylab('New Cases') + theme(legend.position = "none") +
+           ggtitle('India: Covid New Cases (Daily)'))
 
 ggplotly(ggplot(data %>% filter(grepl('India', location)), 
-  aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
-  xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
-  ggtitle('India: Covid New Deaths (Daily)'))
+                aes(x=date, y=new_deaths, color="#990239")) + geom_point() +
+           xlab('Time') + ylab('New Deaths') + theme(legend.position = "none") +
+           ggtitle('India: Covid New Deaths (Daily)'))
+
+
+
+continents <- c('World', 'North America', 'Europe', 'European Union', 'South America',
+                'Asia', 'Africa')
+
+top_10 <- (countries%>%arrange(desc(total_deaths)) %>% 
+             filter(!(location %in% continents)))$location[1:10]
+
+top_20 <- (countries%>%arrange(desc(total_deaths)) %>% 
+             filter(!(location %in% continents)))$location[1:20]
+
+top_10
+top_20
+
 
 # New deaths smoothed, top 10 countries
 ggplot(subset(data, location %in% top_10), aes(x=date, y=new_deaths_smoothed, color=location)) + geom_line() +
-           xlab('Time') + ylab('New Deaths') + ylim(0,4000) + ggtitle('Daily Covid Deaths Worldwide') +
-           theme(legend.position = "bottom") + scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1))))
+  xlab('Time') + ylab('New Deaths') + ylim(0,4000) + ggtitle('Daily Covid Deaths Worldwide') +
+  theme(legend.position = "bottom") + scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1))))
 ggplotly(ggplot(subset(data, location %in% top_10), aes(x=date, y=new_deaths_smoothed, color=location)) + geom_line() +
            xlab('Time') + ylab('New Deaths') + ylim(0,4000) + ggtitle('Daily Covid Deaths Worldwide') +
            theme(legend.position = "bottom") + scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1)))))
 
 # New deaths/million smoothed, top 20 countries
 ggplot(subset(data, location %in% top_20), aes(x=date, y=new_deaths_smoothed_per_million, color=location)) + geom_line() +
-           xlab('Time') + ylab('New Deaths/Million') + ylim(0,25) + ggtitle('Daily Covid Deaths per Million, Worldwide') +
-           theme(legend.position = "bottom") + scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1))))
+  xlab('Time') + ylab('New Deaths/Million') + ylim(0,25) + ggtitle('Daily Covid Deaths per Million, Worldwide') +
+  theme(legend.position = "bottom") + scale_x_date(date_breaks = '1 month',date_labels = "%b%y", limits = as.Date(c('2020-02-01',(Sys.Date()-1))))
